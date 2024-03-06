4e54d61d5ad26e75af8b34cdeb2ebf6145288e65
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-03-06 17:15:25 +0100

    Added healthy check controller

---
+++ src/main/java/dev/naimsulejmani/locationservicegr1/controllers/HealthController.java
@@ -0,0 +1,15 @@
+package dev.naimsulejmani.locationservicegr1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/health")
+public class HealthController {
+
+    @GetMapping("ping")
+    public String pingMe() {
+        return "pong";
+    }
+}
